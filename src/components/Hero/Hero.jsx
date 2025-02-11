3651e623b2f4012f26fbd6c41d70c019c53dfd74
Kelvin Mwangi <kevinomwangi@gmail.com>
2025-02-11 20:08:32 +0300

    initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Hero.css";
+import me from "../../assets/me.png";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <img src={me} alt="" />
+      <h1>
+        <span>I'm Kev,</span> A software developer based in Nairobi, Kenya
+      </h1>
+      <p>
+        Motivated Information Technology professional with a comprehensive
+        foundation in software development, network administration, and database
+        management. Skilled in web development technologies including HTML, CSS,
+        JavaScript, Bootstrap, and Tailwind CSS. Proficient in programming
+        languages Java and JavaScript, with hands-on experience in MySQL
+        database management
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect</div>
+        <div className="hero-resume">My Resume</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;

c554908a5cbd0e5c382fe0a8a12a731ace4aebcb
Kelvin Mwangi <kevinomwangi@gmail.com>
2025-04-08 22:02:04 +0300

    my portfolio update

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,41 +1,40 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project1_img from "../assets/project_1.PNG";
+import project2_img from "../assets/project_2.PNG";
+import project3_img from "../assets/project_3.PNG";
+import project4_img from "../assets/project_4.PNG";
+import project5_img from "../assets/project_5.PNG";
 
 const mywork_data = [
-    {
-        w_no:1,
-        w_name:"Web design",
-        w_img:project1_img
-    },
-    {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project2_img
-    },
-    {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
-    },
-    {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
-    },
-    {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
-    },
-]
- 
+  {
+    w_no: 1,
+    w_name: "Developer Portfolio",
+    w_img: project1_img,
+    repo_link: "https://github.com/Kelv11/My-portfolio",
+  },
+  {
+    w_no: 2,
+    w_name: "AI Assistant Interface",
+    w_img: project2_img,
+    repo_link: "https://github.com/Kelv11/Google-Gemini-Chatbot",
+  },
+  {
+    w_no: 3,
+    w_name: "Data Analyst Portfolio",
+    w_img: project3_img,
+    repo_link: "https://github.com/Kelv11/portfolio-collins",
+  },
+  {
+    w_no: 4,
+    w_name: "E-Commerce Store",
+    w_img: project4_img,
+    repo_link: "https://github.com/Kelv11/Kicks-shoe-store",
+  },
+  {
+    w_no: 5,
+    w_name: "Quiz App Interface",
+    w_img: project5_img,
+    repo_link: "https://github.com/Kelv11/Quiz-app",
+  },
+];
+
 export default mywork_data;
--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import "./MyWork.css";
-
 import mywork_data from "../../assets/mywork_data";
 import arrow_icon from "../../assets/arrow_icon.svg";
+
 const MyWork = () => {
   return (
     <div id="work" className="mywork">
       <div className="mywork-title">
-        <h1>MyLatest Work</h1>
+        <h1>My Latest Work</h1>
       </div>
       <div className="mywork-container">
         {mywork_data.map((work, index) => {
-          return <img key={index} src={work.w_img} alt="" />;
+          return (
+            <a
+              key={index}
+              href={work.repo_link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="work-link"
+            >
+              <img src={work.w_img} alt={work.w_name} />
+            </a>
+          );
         })}
       </div>
-      <div className="mywork-showmore">
-        <p>Show More</p>
-        <img src={arrow_icon} alt="" />
-      </div>
+      <a
+        href="https://github.com/Kelv11?tab=repositories"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mywork-showmore-link"
+      >
+        <div className="mywork-showmore">
+          <p>Show More</p>
+          <img src={arrow_icon} alt="" />
+        </div>
+      </a>
     </div>
   );
 };
